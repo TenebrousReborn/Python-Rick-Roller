81b93000fefb8640469d80d238a8bc410e777def
MialiYr <89588337+MialiYr@users.noreply.github.com>
2022-10-25 02:35:34 +0200

    Add files via upload

---
+++ MyProjectBasic.py
@@ -0,0 +1,4 @@
+input ("free vbuxs? well go to thispc.com to get your award do you agree please type agree down to agree: ")
+print ("here you go https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+print ("look at you falling for that")
+input ("right? ")

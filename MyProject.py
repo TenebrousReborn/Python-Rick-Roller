f5e6d7ab91410050375bc1ddec0e95cba92043d8
MialiYr <89588337+MialiYr@users.noreply.github.com>
2022-10-25 02:34:17 +0200

    Myproject.pyy
    yeeeeeee

---
+++ MyProject.py
@@ -0,0 +1,9 @@
+#do pip install colorama
+from colorama import Fore
+
+
+input (f"{Fore.RED}free vbuxs? well go to thispc.com to get your award do you agree please type agree down to agree: ")
+print (f"{Fore.GREEN}here you go https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+print (f"{Fore.BLUE}look at you falling for that")
+input (f"{Fore.RED}right? ")
+
